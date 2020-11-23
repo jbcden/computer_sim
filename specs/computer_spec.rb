7ca7f2dad701eafa02030515a2718425a0ad88a2
Jacob Chae <jbcden@gmail.com>
2020-11-23 11:36:46 +0900

    Add note about stack implementation question

--- specs/computer_spec.rb
+++ specs/computer_spec.rb
@@ -4,6 +4,8 @@ require_relative '../lib/computer'
 
 RSpec.describe Computer do
   describe '#set_address' do
+    # TODO: should we add a check that the address
+    # is within the stack?
     it 'sets the address in the stack' do
       computer = Computer.new(100)
 
