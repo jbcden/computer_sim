c291974857e7ffbcda74b7ddf76749a0156b17ea
Jacob Chae <jbcden@gmail.com>
2020-11-23 12:35:05 +0900

    Add initial `insert` spec

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,4 +9,12 @@ class Computer
   def set_address(address)
     @program_counter = address
   end
+
+  # NOTE: currently all commands have at most 1 argument, we could
+  # update the arg argument to handle multiple arguments if necessary.
+  def insert(command_name, arg = nil); end
+
+  def current_instruction
+    "STOP"
+  end
 end
--- specs/computer_spec.rb
+++ specs/computer_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe Computer do
     # TODO: should we add a check that the address
     # is within the stack?
     it 'sets the address in the stack' do
-      computer = Computer.new(100)
+      computer = Computer.new(30)
 
       computer.set_address(22)
 
       expect(computer.program_counter).to eq(22)
+    end
+  end
+
+  describe '#insert' do
+    it 'pushes a command onto the stack' do
+      computer = Computer.new(10)
+
+      computer.set_address(0)
+      computer.insert("STOP")
+
+      computer.set_address(0)
+      expect(computer.current_instruction).to eq("STOP")
     end
   end
 end
