93816d5c7ac7f0f17876fbba2e29e3ed1238bf17
Jacob Chae <jbcden@gmail.com>
2020-11-23 17:43:13 +0900

    `Computer` can now run a program
    With this commit, the `Computer` class can now run a program which
    consists of the PUSH, PRINT, and STOP commands. The main improvement
    that could be done here would be to construct the command objects when
    they are inserted into the stack to avoid needing to iterate over a
    mostly empty stack.
    
    Another caveat of this implementation is that if no STOP command is
    present, this program will run forever.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative './command_builder'
+require_relative './context'
+
 class Computer
   attr_reader :program_counter, :stack
   alias instruction_pointer program_counter
@@ -27,5 +30,23 @@ class Computer
 
   def current_instruction
     @stack[@program_counter]
+  end
+
+  def execute
+    @stack = @stack.map do |command_str|
+      next nil unless command_str
+
+      command, arg = command_str.split(" ")
+      CommandBuilder.build(command, arg)
+    end
+
+    loop do
+      context = Context.new(stack: @stack, pc: @program_counter)
+
+      new_context = @stack[@program_counter].run(context)
+
+      @stack = new_context.stack
+      @program_counter = new_context.pc
+    end
   end
 end
